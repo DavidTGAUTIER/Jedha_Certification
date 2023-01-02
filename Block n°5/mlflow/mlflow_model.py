fd7fb9435c62f658ebc606ecc676db47bd26c8b1
david <davidtiffeneau@live.fr>
2023-01-02 16:41:30 +0100

    add file

--- Block n°5/mlflow/mlflow_model.py
+++ Block n°5/mlflow/mlflow_model.py
@@ -10,3 +10,20 @@ from sklearn.metrics import r2_score
 from sklearn.pipeline import Pipeline
 
 from xgboost import XGBRegressor
+
+link = os.environ["APP_URI"]
+mlflow.set_tracking_uri(link)
+print(f'Lien du site mlflow : {link}')
+
+def loading():
+    print('Importation des données')
+    data = pd.read_csv('../src/get_around_pricing_project.csv')
+    data = data.iloc[:,1:]
+    return data
+
+def remove_outliers(data, cols):
+    for col in cols:
+        if col == 'engine_power':
+            outliers = 
+
+def preprocessing(data):
