f3a9e71a274439da4a3333d580f113bce2497c6f
david <davidtiffeneau@live.fr>
2023-01-05 15:29:05 +0100

    modify func

--- Block n°5/app/pages/threshold.py
+++ Block n°5/app/pages/threshold.py
@@ -108,17 +108,34 @@ st.subheader("Calcul du seuil de rentabilité")
 st.markdown("""En multipliant la somme des retards d'une certaine tranche horaire obtenus par le prix moyen d'une course, on peut connaitre le montant total des retards de cette tranche horaire.
 Nous allons pouvoir afficher sur un graphique les pertes et leur evolution au cours de 24h""")
 
+h = 24
 # on ne prend pas les outliers pour avoir un nombre total de sample plus exacte (nous ne connaissons pas les raisons des NaN)
 delays_without_nan = data_delays.dropna(subset=['delay_at_checkout_in_minutes'])
+prix_moyen_course = data_pricing['rental_price_per_day'].mean()
+prix_moyen_course_par_heure = prix_moyen_course / (24*60)
 
-interval_range = np.arange(0, 60*24, step=30)
-prix_minute = prix_moyen_course / (24*60)
+annulations = (delays_without_nan['state'] == 'canceled').sum()
+pertes_totales = prix_moyen_course * annulations
+
+only_delays = delays_without_nan[delays_without_nan['delay_at_checkout_in_minutes'] > 0]
+total_retards = only_delays.loc[:, 'delay_at_checkout_in_minutes'].sum()
+revenu_retard = int(total_retards * prix_moyen_course_par_heure)
+
+heures_rentable = (revenu_retard / pertes_totales) * 24
 total_revenus_retard = []
 
+interval_range = np.arange(0, 60*24, step=30)
 for interval in interval_range:
     threshold = delays_without_nan[delays_without_nan['delay_at_checkout_in_minutes'] > interval]
     somme_retard = threshold.loc[:, 'delay_at_checkout_in_minutes'].sum()
-    somme_revenus_retard = somme_retard * prix_minute
+    somme_revenus_retard = somme_retard * prix_moyen_course_par_heure
     total_revenus_retard.append(somme_revenus_retard)
     
 total_revenus_retard.reverse()
+h = 24
+fig = make_subplots(rows = 1, cols = 2, subplot_titles = ("Cout de revient des annulations sur 24h", "Distribution des annulations"))
+fig.add_trace(go.Scatter(x=interval_range/60, y=total_revenus_retard), row=1, col=1)
+fig.add_hline(y=int(pertes_totales/h*heures_rentable))
+fig.add_trace(go.Histogram(x=total_revenus_retard),row=1, col=2)
+fig.update_layout(title = go.layout.Title(text = "Distribution des retards", x=0.5), showlegend=False)
+st.plotly_chart(fig, use_container_width=True)
